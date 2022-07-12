b4ea2f4a3cecf632fa2d8ae1fd70ce012b109e6b
Nikolay Chumakov <gerrronimo@Nikolays-MBP.Dlink>
2022-07-12 21:35:00 +0300

    Implemented architecture of ui-kit

--- packages/apps/core/craco.config.js
+++ packages/apps/core/craco.config.js
@@ -2,15 +2,17 @@ const path = require("path");
 const { getLoader, loaderByName } = require("@craco/craco");
 
 const packages = [];
-const libs = [
-  "booktrina-ui"
-];
-libs.map(lib => packages.push(path.join(__dirname, `../../libs/${lib}`)))
+const libs = ["booktrina-ui/src"];
+libs.map((lib) => packages.push(path.join(__dirname, `../../libs/${lib}`)));
+console.log("\x1b[36m%s\x1b[0m", `Added custom libraries: ${libs}`);
 
 module.exports = {
   webpack: {
     configure: (webpackConfig, arg) => {
-      const {isFound, match} = getLoader(webpackConfig, loaderByName("babel-loader"));
+      const { isFound, match } = getLoader(
+        webpackConfig,
+        loaderByName("babel-loader")
+      );
       if (isFound) {
         const include = Array.isArray(match.loader.include)
           ? match.loader.include
---
+++ packages/libs/booktrina-ui/src/Typography/Title/Title.test.tsx
@@ -0,0 +1,7 @@
+import { Title } from './Title';
+
+describe('try to render a Title component', () => {
+  it('is truthy', () => {
+    expect(Title).toBeTruthy();
+  });
+});
